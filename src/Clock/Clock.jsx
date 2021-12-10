56e83cde6912ad52c900a2d9070db80a8b4642fa
NicolasDrapeau <drapeau.n@sfeir.com>
2021-12-10 11:00:50 +0100

    init

---
+++ src/Clock/Clock.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+export class Clock extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            date : new Date()
+        }
+        this.timer = null
+
+    }
+
+    componentDidMount(){
+        this.timer = setInterval(() => this.setState({
+            date: new Date()
+        }), 1000)
+    }
+
+    componentWillUnmount(){
+        window.clearInterval(this.timer);
+    }
+
+    render(){
+        return <div className="clock">{this.state.date.toLocaleString()}</div>
+    }
+}
